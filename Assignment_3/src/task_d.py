cc54daccba7c205e29c7cb229c2dbfd8cdf5bb73
thealphadollar <thealphadollar>
2020-03-29 12:34:50 +0530

    complete task d

--- Assignment_3/src/task_d.py
+++ Assignment_3/src/task_d.py
@@ -8,18 +8,18 @@ from task_b import AgglomerativeClustering
 from task_c import KMeans
 
 def main():
-    with open('../data/tdidf_vector.pkl', 'rb') as f_open:
+    with open('data/tdidf_vector.pkl', 'rb') as f_open:
         tfidf_matrix = pkl.load(f_open)
     pca = PCA(n_components=100)
     tfidf_array = pca.fit_transform(tfidf_matrix.toarray())
     # tfidf_array.shape
-
+    
     # Agglomerative Reduced
-    agglo = AgglomerativeClustering(path="../clusters/agglomerative_reduced.txt")
+    agglo = AgglomerativeClustering(path="clusters/agglomerative_reduced.txt")
     agglo.fit(tfidf_matrix.toarray())
 
     # KMeans Reduced
-    kmeans = KMeans(path="../clusters/kmeans_reduced.txt")
+    kmeans = KMeans(path="clusters/kmeans_reduced.txt", iters=1000)
     kmeans.fit(tfidf_matrix.toarray())
 
 if __name__=="__main__":
